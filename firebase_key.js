af6f2793f9da8c357aaf542a761d30d0dce03bf7
Nwele Uchenna <nweleuchenna2004@gmail.com>
2021-10-22 03:25:59 +0100

    fixed api key issues

---
+++ firebase_key.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCbeOJAswLyZYrH5xPwX6xC62HYCnGBgV0",
+  authDomain: "whatsapp-next-4be5d.firebaseapp.com",
+  projectId: "whatsapp-next-4be5d",
+  storageBucket: "whatsapp-next-4be5d.appspot.com",
+  messagingSenderId: "218883995278",
+  appId: "1:218883995278:web:8f372e13594c76c8d53bdf",
+  measurementId: "G-JPGP3Q1TDD"
+};
+
+export default firebaseConfig
